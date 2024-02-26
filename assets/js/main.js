ce606a050426bd272a94f02872b3e1205164198b
Zied1981 <zkhemiri@gmail.com>
2024-02-26 16:25:59 +0100

    2 push

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+//to do
+// textinput value holen
+//error mit output
+//sprache select vslue holen
+//sort value holen
+
+let SearchBtn = document.querySelector(".button");
+let outputText = document.querySelector(".errortext");
+
+const fetchNewsData = (event) => {
+  event.preventDefault();
+
+  let inputText = document.querySelector("#inputText").value;
+  console.log(inputText);
+  if (inputText.length <= 0) {
+    console.log("error");
+    outputText.innerHTML = `<p>please enter a content<p>`;
+    return;
+  }
+  let selectedlang = document.querySelector("#language").value;
+  console.log(selectedlang);
+
+  let sortedVal = document.querySelector("#sortieren").value;
+  console.log(sortedVal);
+  let contentOutput = document.querySelector(".contentOutput");
+  console.log(contentOutput);
+
+  fetch(
+    `https://newsapi.org/v2/everything?q=${inputText}&language=${selectedlang}&sortBy=${sortedVal}&apiKey=74d16d926146498b9f2453203d57af5e`
+  )
+    .then((response) => response.json())
+    .then((everyData) => {
+      console.log(everyData);
+
+      contentOutput.innerHTML = "";
+      outputText.innerHTML = "";
+
+      everyData.articles.forEach((element) => {
+        let title = element.title;
+        console.log(title);
+        let articleContent = element.content;
+        console.log(articleContent);
+        let urlImG = element.urlToImage;
+        console.log(urlImG);
+
+        let moreInfoBTn = element.url;
+
+        contentOutput.innerHTML += `<article>
+        <h3>${title}</h3>
+        <p>${articleContent}</p>
+        <img src="${urlImG}" alt="">
+        <a href="${moreInfoBTn}"target="_blank">zum artikel</a>
+        </article>`;
+      });
+    })
+    .catch(console.log("error im hauptfetch"));
+};
